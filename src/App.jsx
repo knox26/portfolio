dd4271d9387ae86934c71f5611f1df6d39d69268
knox26 <bvjadhav7558@gmail.com>
2025-01-31 20:30:50 +0530

    modified:   src/App.jsx 	modified:   src/main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ import ContactMe from "./components/ContactMe.jsx";
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="flex items-center justify-center  ">
-        <div className="mx-10  w-[90vw] sm:w-[65vw] md:w-[750px]">
+      <div className="flex items-center justify-center">
+        <div className="mx-10 w-[90vw] sm:w-[65vw] md:w-[750px]">
           <NavBar />
           <Routes>
             <Route path="/" element={<Home />} />
